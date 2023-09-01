5ee43ac81895509c77e09c9e4fa1de4f1a4d95d3
paula <paula@omniesolutions.com>
2023-09-01 11:18:59 +0530

    Space added

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,7 +5,7 @@ import { TestModule } from 'src/test/test.module';
 @Module({
   imports: [TestModule],
   providers: [UserService],
-  //alow service from one module to be used in other
+  // alow service from one module to be used in other
   exports: [UserService],
 })
 export class UserModule {}
