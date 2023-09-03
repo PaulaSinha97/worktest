4e61dabfbec1e077d1d7d4da9a1e1bb3ef6a6005
paula <paula@omniesolutions.com>
2023-09-04 00:56:06 +0530

    Fix formatting

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // For FE to get this cookie we need to pass passthorugh and enable here cors
   app.enableCors({
-    origin:'http://localhost:3000',
-    credentials:true})
-    app.use(cookieParser());
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist:true
-    })
-  )
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
